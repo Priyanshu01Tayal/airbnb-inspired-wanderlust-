a905e5980f59179732c1eb8bf43d07d95a317472
Priyanshu0107 <priyanshutayal35@gmail.com>
2026-02-14 22:25:24 +0530

    Added layouts, navbar, footer, edit page and styling updates

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const mongoose=require("mongoose")
 
 const Listing=require("./models/listing.js")
 
-const path=require("path");
+const path=require("path")
+const methodOverride=require("method-override")
+const ejsMate = require("ejs-mate");
+
 const Mongo_url="mongodb://127.0.0.1:27017/wanderlust"
 main().then(()=>{
     console.log("connectes")
@@ -19,6 +22,10 @@ async function main()
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/listings",async (req,res)=>{
    const allListings=await Listing.find({});
@@ -44,9 +51,26 @@ await newListing.save();
 res.redirect("/listings")
 })
 
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+ const listing=await Listing.findById(id);
+res.render("listings/edit",{listing});
+})
 
+app.put("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+ await Listing.findByIdAndUpdate(id,{...req.body.listing})
 
+ res.redirect("/listings")
+})
 
+app.delete("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id)
+
+    res.redirect("/listings");
+
+})
 app.get("/",(req,res)=>{
     res.send("connection succesful")
 })
