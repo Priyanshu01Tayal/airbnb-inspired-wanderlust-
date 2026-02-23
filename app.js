f5e7afa109af689ce1bb8545f9ba7377b86f5db0
Priyanshu0107 <priyanshutayal35@gmail.com>
2026-02-23 16:57:44 +0530

    Added review model and updated listing & show page

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const ejsMate = require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
 const {ListingSchema, listingSchema}=require("./schema.js");
+const Review=require("./models/review.js")
+
 const Mongo_url="mongodb://127.0.0.1:27017/wanderlust"
 main().then(()=>{
     console.log("connectes")
@@ -84,6 +86,20 @@ app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     res.redirect("/listings");
 
 }))
+// REVIEW
+
+app.post("/listings/:id/reviews",async(req,res)=>{
+ let listing=await Listing.findById(req.params.id);
+ let newReview=new Review(req.body.review);
+ listing.reviews.push(newReview);
+ await newReview.save();
+ await listing.save();
+ res.redirect(`/listings/${listing._id}`)
+})
+
+
+
+
 app.get("/",(req,res)=>{
     res.send("connection succesful")
 })
