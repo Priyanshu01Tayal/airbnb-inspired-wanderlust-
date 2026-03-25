0c5a652153587501dee45a802ae396e1d05d48ab
Priyanshu0107 <priyanshutayal35@gmail.com>
2026-03-25 22:00:16 +0530

    review authentication

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 // const Listing=require("./models/listing")
 const ExpressError=require("./utils/ExpressError.js");
 const Listing=require("./models/listing.js")
+const Review=require("./models/review.js")
 const {listingSchema, reviewSchema } = require("./schema.js");
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -51,4 +52,15 @@ module.exports.validateReview = (req, res, next) => {
 
     next();
 };
+
+module.exports.isReviewAuthor=async(req,res,next)=>{
+    let {reviewId}=req.params;
+    let review=await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id));
+    {r
+        req.flash("error","you do not have a permission ")
+          return res.redirect("/listings")
+    }
+    next();
+}
 
--- routes/review.js
+++ routes/review.js
@@ -7,12 +7,13 @@ const ExpressError = require("../utils/ExpressError.js");
 const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 
-const {validateReview}=require("../middleware.js")
+const {validateReview,isLoggedIn, isReviewAuthor }=require("../middleware.js")
 
 
-router.post("/", validateReview, wrapAsync(async (req,res)=>{
+router.post("/", isLoggedIn,validateReview, wrapAsync(async (req,res)=>{
  let listing=await Listing.findById(req.params.id);
  let newReview=new Review(req.body.review);
+newReview.author=req.user._id;
  listing.reviews.push(newReview);
  await newReview.save();
  await listing.save();
@@ -21,11 +22,11 @@ router.post("/", validateReview, wrapAsync(async (req,res)=>{
 }))
 //dlete revoiew
 
-router.delete("/:reviewId", wrapAsync(async (req,res)=>{
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(async (req,res)=>{
     let {id,reviewId}=req.params;
     await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
     await Review.findByIdAndDelete(reviewId);
     req.flash("success","Review deleted successfully");
     res.redirect(`/listings/${id}`)
 })) 
-module.exports=router;
+module.exports=router; 
